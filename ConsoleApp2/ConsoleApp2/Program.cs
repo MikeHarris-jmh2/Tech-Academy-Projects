6ec8777767df05bc2fc7dfb89d244b73beb1a927
Mike Harris <Mikeharris.jmh3@gmail.com>
2018-12-17 14:01:26 -0800

    Creating Daily Report
    Creating a daily report

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string courseProgression = Console.ReadLine();
+
+            Console.WriteLine("What Page Number are you on?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? True or False");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+           
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positive = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
