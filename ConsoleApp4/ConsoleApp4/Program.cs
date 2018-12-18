9429419a906023fabd39c56778d6d53e8ba988b3
Mike Harris <Mikeharris.jmh3@gmail.com>
2018-12-18 12:09:25 -0800

    Fixes to Exercise 67
    Fixing the Exercise 67

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -12,40 +12,42 @@ namespace ConsoleApp4
 
             Console.WriteLine("Hourly Rate?");
             int person1Hourly = Convert.ToInt32(Console.ReadLine());
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Hours worked per week?");
             int person1WorkWeek = Convert.ToInt32(Console.ReadLine());
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Person 2");
 
             Console.WriteLine("Hourly Rate?");
             int person2Hourly = Convert.ToInt32(Console.ReadLine());
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Hours worked per week?");
             int person2WorkWeek = Convert.ToInt32(Console.ReadLine());
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Weekly salary of Person 1: ");
             int person1WeeklySalary = person1Hourly * person1WorkWeek;
             Console.WriteLine(person1WeeklySalary);
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Weekly salary of Person 2: ");
             int person2WeeklySalary = person2Hourly * person2WorkWeek;
             Console.WriteLine(person2WeeklySalary);
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Does Person 1 make more money than Person 2?");
             if(person1WeeklySalary > person2WeeklySalary)
             {
                 Console.WriteLine("True");
+                Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("False");
+                Console.ReadLine();
             }
         }
     }
