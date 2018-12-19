27e43839a8cd87f295a098bc98d354833bd47422
Mike Harris <Mikeharris.jmh3@gmail.com>
2018-12-19 14:32:03 -0800

    Exercise  Step 94
    Completed excersie on step 94

---
+++ booleandoandwhilestatements/booleandoandwhilestatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace booleandoandwhilestatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = 0;
+            
+            while(number < 25)
+            {
+                Console.WriteLine(number);
+                Console.ReadLine();
+                number++;
+                
+            }
+
+
+            int number2 = 0;
+            do
+            {
+                Console.WriteLine(number2);
+                number2++;
+            } while (number2 < 10);
+
+
+        }
+    }
+}
