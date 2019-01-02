7d900e68ea06526432ec812d8ee0468b8c480ee6
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-02 15:04:18 -0800

    Classes and Objects
    Exercise methods for operators not working

---
+++ MathOperations/MathOperations/MathStuff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class MathStuff
+    {
+        public int userNumber { get; set; };
+
+        public addNumber()
+        {
+            Console.WriteLine(userNumber + 2);
+        }
+
+        public subtractNumber()
+        {
+            Console.WriteLine(userNumber - 2);
+        }
+
+        public divideNumber()
+        {
+            Console.WriteLine(userNumber / 2);
+        }
+    }
+}
---
+++ MathOperations/MathOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Which number would you like to run through the Math Guantlet?");
+           public int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            MathStuff.addNumber(userNumber);
+
+
+        }
+    }
+}
