308420208c2c83083bd729934e4ed403e430600b
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-04 13:29:40 -0800

    Exercise 117
    Completed exercise 117

---
+++ MathExercise3/MathExercise3/MathOverload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExercise3
+{
+    class MathOverload
+    {
+        public void DividebyTwo(out int a)
+        {
+            a = 2;
+            int x = a / 2;
+            
+        }
+    }
+}
---
+++ MathExercise3/MathExercise3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExercise3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please provide a number you would like to be divided by 2");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            MathOverload numberOverload = new MathOverload();
+            numberOverload.DividebyTwo(out userNumber);
+            Console.WriteLine(userNumber);
+            Console.ReadLine();
+
+        }
+    }
+}
