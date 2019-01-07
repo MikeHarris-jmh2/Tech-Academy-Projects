317e2b76472022413ed603ff79a0603b32cd2aa0
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-06 17:39:55 -0800

    BooleanStatement
    Boolean exercise 94

---
+++ booleanWhileStatement/booleanWhileStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booleanWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            int number2 = 0;
+
+            while (number < 10)
+            {
+                Console.WriteLine("Hello There", number);
+                Console.ReadLine();
+                number++;
+
+            }
+
+            do
+            {
+                Console.WriteLine("The number is {0}", number2);
+                Console.ReadLine();
+                number2++;
+            } while (number2 < 12);
+
+        }
+    }
+}
