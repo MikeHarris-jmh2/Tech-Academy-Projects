b3f7af827e480d4cf312d57a0a4cae4116cb9559
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-06 18:59:48 -0800

    Exercise 115
    Completed Exercise 115

---
+++ ConsoleApp6/ConsoleApp6/OptionalParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class OptionalParameter
+    {
+        public int divideNumber(int a, int b = 0)
+        {
+            int c = a / 2;
+            return c;
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please provide another number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            OptionalParameter parameter = new OptionalParameter();
+            int finalNumber = parameter.divideNumber(firstNumber, secondNumber);
+            Console.WriteLine(finalNumber);
+
+
+
+        }
+    }
+}
