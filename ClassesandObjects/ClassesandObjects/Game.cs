87deadf4ec30e49d3faa593d51a4b349cb21df55
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-08 14:33:30 -0800

    File in and out exercise
    File in and out exercise

--- ClassesandObjects/ClassesandObjects/Game.cs
+++ ClassesandObjects/ClassesandObjects/Game.cs
@@ -8,9 +8,12 @@ namespace ClassesandObjects
 {
     public abstract class Game
     {
+        private List<Player> _players = new List<Player>();
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>();
+
         public List<Player> Players { get; set; }
         public string Name { get; set; }
-        public Dictionary<Player, int> Bets { get; set; }
+        public Dictionary<Player, int> Bets { get {return _bets; } set { _bets = value; } }
 
 
         public abstract void Play();
---
+++ FileIOExercise/FileIOExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:C:\Users\Student\logs\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+
+        }
+    }
+}
