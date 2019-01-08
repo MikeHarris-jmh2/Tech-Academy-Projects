37f36cbd3382317c46892876c361d05e0fe8afd7
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-08 15:11:11 -0800

    DateTime exercise
    DateTime exercise step 156

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Please provide a number: ");
+            int usernumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The time will be " + date.AddHours(usernumber) + "in {0}", usernumber);
+            Console.ReadLine();
+        }
+    }
+}
