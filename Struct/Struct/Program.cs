03cb25560b9650633a5e5f4e78712a03949226f1
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-01-09 16:23:40 -0800

    Struct Exercise
    Struct exercise

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal Amount = 200;
+
+            Console.WriteLine(Amount);
+            Console.Read();
+        }
+    }
+}
