cd0df69e7414413d92bd1071446485581af91074
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-02-01 15:44:26 -0800

    Exercise 129
    Exercise 129

--- EmployeePerson/EmployeePerson/Program.cs
+++ EmployeePerson/EmployeePerson/Program.cs
@@ -14,12 +14,30 @@ namespace EmployeePerson
     {
         public static void Main(string[] args)
         {
-            Employee employee = new Employee();
-            employee.FirstName = "Sample";
-            employee.LastName = "Student";
-            employee.SayName();
+            List<Person> employees = new List<Person>();
+            employees.Add(new Employee());
+            employees[0].FirstName = "Sample";
+            employees[0].LastName = "Student";
+            employees.Add(new Employee());
+            employees[1].FirstName = "Example";
+            employees[1].LastName = "Member";
 
-            
+            foreach(Employee person in employees)
+            {
+                Console.WriteLine(person.FirstName + " " + person.LastName);
+
+                if (person.EmployeeId == person.EmployeeId)
+                {
+                    Console.WriteLine("These employees are the same person");
+                } else
+                {
+                    Console.WriteLine("These employees are not the same");
+                }
+            }
+
+            Console.ReadLine();
+
+
         }       
     }
 }
