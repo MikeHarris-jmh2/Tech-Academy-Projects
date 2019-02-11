ebd8907cb9807d78709bf13d1e94c284186bb64c
Mike Harris <Mikeharris.jmh3@gmail.com>
2019-02-11 13:37:44 -0800

    Exercise124
    Exercise124

---
+++ Exercise124/Exercise124/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise124
+{
+    class Employee : Person
+    {
+
+        public int EmployeeId { get; set; }
+        public List<Person> Employees { get; set; }
+
+
+        public override void SayName()
+        {
+            Console.WriteLine("Please provide a First Name:");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please provide a Last Name:");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+
+        }
+    }
+}
